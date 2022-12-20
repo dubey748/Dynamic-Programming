e2be6f2d0073efeba820ce60e8dfccc49be456ac
dubey748 <dubeyabhishek748@gmail.com>
2022-12-21 01:10:41 +0530

    https://www.youtube.com/watch?v=ntCGbPMeqgg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=5&ab_channel=AdityaVerma

--- KnapsackTop_Down.js
+++ KnapsackTop_Down.js
@@ -1,25 +1,17 @@
 // Using Recursion  + memoized
 const knapsack = (wt, val, w, n) => {
-  
-    let t = new Array(n + 1);
-    for (let i = 0; i <= n; i++)
-    {
-        t[i] = new Array(w + 1);
-        for (let j = 0; j <= w; j++)
-        {
-            if (i == 0 || w == 0)
-                t[i][j] = 0;
-            else if (wt[i - 1] <= j)
-                t[i][j]
-                    = Math.max(val[i - 1]
-                     + t[i - 1][j - wt[i - 1]],
-                     t[i - 1][j]);
-            else
-                t[i][j] = t[i - 1][j];
-        }
+  let t = new Array(n + 1);
+  for (let i = 0; i <= n; i++) {
+    t[i] = new Array(w + 1);
+    for (let j = 0; j <= w; j++) {
+      if (i == 0 || j == 0) t[i][j] = 0;
+      else if (wt[i - 1] <= j)
+        t[i][j] = Math.max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
+      else t[i][j] = t[i - 1][j];
     }
+  }
 
-    return t[n][w];
+  return t[n][w];
 };
 
 console.log(knapsack([20, 30, 40, 70], [70, 80, 90, 200], 60, 4));
