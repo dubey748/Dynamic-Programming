34f8b8ffc784ebc9a98abe3c1bfce17122782539
dubey748 <dubeyabhishek748@gmail.com>
2023-01-03 00:46:10 +0530

    https://www.youtube.com/watch?v=-GtpxG6l_Mc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=10&ab_channel=AdityaVerma

--- MinimumSubsetSumDifference.js
+++ MinimumSubsetSumDifference.js
@@ -16,28 +16,18 @@ const subset = (arr, sum, len) => {
   }
   let vector = [];
   
-  let range = Math.abs(sum / 2);
-  console.log("range " + range)
-  if(range%2==0){
-    for (let i = len; i <= len; i++) {
-        for (let j = 0; j < range+1 ; j++) {
-          if (t[i][j] == true) {
-            vector.push(j);
-          }
-        }
-      }
-  }
-  else{
-    for (let i = len; i <= len; i++) {
-        for (let j = 0; j < range ; j++) {
-          if (t[i][j] == true) {
-            vector.push(j);
-          }
-        }
-      }
-  }
  
-  console.log(vector);
+  
+    for (let i = len; i <=len; i++) {
+        for (let j = 0; j < sum ; j++) {
+          if (t[i][j] == true) {
+            vector.push(j);
+          }
+        }
+      }
+  
+ 
+  
   return vector;
 };
 const MinimumSubsetSumDifference = (arr) => {
@@ -47,32 +37,34 @@ const MinimumSubsetSumDifference = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
   }
-  console.log(sum);
+  
 
   if (sum <= 0) {
     for (let i = 0; i < arr.length; i++) {
       min = Math.min(min, arr[i]);
     }
-    console.log(min)
+    
 
     for (let i = 0; i < arr.length; i++) {
       arr[i] += Math.abs(min);
     }
-    console.log(arr)
+    
   }
   for (let i = 0; i < arr.length; i++) {
     add = add + arr[i];
   }
-  console.log(add);
+  
 
   let mini = Number.MAX_SAFE_INTEGER;
 
   let vector = subset(arr, add, arr.length);
+let len=Math.round((vector.length)/2)
 
-  for (let i = 0; i < vector.length; i++) {
+
+  for (let i = 0; i < len; i++) {
     mini = Math.min(mini, Math.abs(add - 2 * vector[i]));
-    console.log(mini);
+    
   }
   return mini;
 };
-console.log(MinimumSubsetSumDifference([76,8,45,20,74,84,28,1]));
+console.log(MinimumSubsetSumDifference([3,9,7,3]));
