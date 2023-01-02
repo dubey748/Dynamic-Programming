263da25e0bfe7eb928f6020d79d4c536480c2960
dubey748 <dubeyabhishek748@gmail.com>
2023-01-03 03:12:39 +0530

    Target Sum- https://www.youtube.com/watch?v=Hw6Ygp3JBYw&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=12&ab_channel=AdityaVerma

--- Countthenumberofsubsetwithagivendifference.js
+++ Countthenumberofsubsetwithagivendifference.js
@@ -1,38 +1,29 @@
 const subset = (arr, sum, len) => {
-    let t = new Array(len + 1);
-    for (let i = 0; i <= len; i++) t[i] = new Array(sum + 1);
-    for(let i=0;i<=len;i++) t[i][0]=true;
-    for(let i=0;i<=sum;i++) t[0][i]=false;
-    t[0][0]=true
-    for(let i=1;i<=len;i++){  
-      for(let j=1; j<=sum;j++){
-          if(arr[i-1]<=j){ 
-              t[i][j]=t[i-1][j-arr[i-1]] + t[i-1][j]  
-             
-              
-             
-          }
-          else{
-              t[i][j]=t[i-1][j]  
-              
-          }
-         
+  let t = new Array(len + 1);
+  for (let i = 0; i <= len; i++) t[i] = new Array(sum + 1);
+  for (let i = 0; i <= len; i++) t[i][0] = true;
+  for (let i = 0; i <= sum; i++) t[0][i] = false;
+  t[0][0] = 1;
+  for (let i = 1; i <= len; i++) {
+    for (let j = 0; j <= sum; j++) {
+      if ((arr[i - 1] || 0) > j) {
+        t[i][j] = t[i - 1][j] || 0;
+      } else {
+        t[i][j] = (t[i - 1][j] || 0) + (t[i - 1][j - arr[i - 1]] || 0);
       }
     }
-    return t[len][sum]
-    
-  };
-const Countthenumberofsubsetwithagivendifference = (arr, diff) => {
- let sum=0;
- for(let i=0;i<arr.length;i++){
-    sum+=arr[i];
- }
- let s2 = (sum+diff)
- if(s2%2==0){
-    let s2=(sum+diff)/2
-    return subset(arr, Math.round(s2), arr.length)
- }
- else return 0
-}
+  }
+  return t[len][sum];
+};
+var findTargetSumWays = function (arr, diff) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
- console.log(Countthenumberofsubsetwithagivendifference([1,1,2,3], 2))
+  if (sum - diff < 0 || (sum - diff) % 2 == 1 || sum + diff < 0) return 0;
+
+  let s1 = (sum + diff) / 2;
+  return subset(arr, Math.abs(s1), arr.length);
+};
+console.log(findTargetSumWays([100, 100], -400));
