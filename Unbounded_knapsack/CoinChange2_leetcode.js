2a6365e7004d7bef20e9ff68a4b95f48fad0ff79
dubey748 <dubeyabhishek748@gmail.com>
2023-01-04 04:35:47 +0530

    https://www.youtube.com/watch?v=I4UR2T6Ro3w&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=15&ab_channel=AdityaVerma

---
+++ Unbounded_knapsack/CoinChange2_leetcode.js
@@ -0,0 +1,34 @@
+const subset = (arr, sum, len) => {
+    let t = new Array(len + 1);
+    for (let i = 0; i <= len; i++) t[i] = new Array(sum + 1);
+    for(let i=0;i<=len;i++) t[i][0]=true;
+    for(let i=0;i<=sum;i++) t[0][i]=false;
+    t[0][0]=true
+    for(let i=1;i<=len;i++){  
+      for(let j=1; j<=sum;j++){
+          if(arr[i-1]<=j){ 
+              t[i][j]=t[i][j-arr[i-1]] + t[i-1][j]  
+             
+              
+             
+          }
+          else{
+              t[i][j]=t[i-1][j]  
+              
+          }
+         
+      }
+    }
+    return t[len][sum]
+    
+  };
+  const coinChange = (arr, sum) => {
+    len = arr.length;
+    let ans=subset(arr, sum, len);
+    if(ans==false){
+        return 0;
+    }
+    return ans
+  };
+  
+  console.log(coinChange([1,2,5], 5));
