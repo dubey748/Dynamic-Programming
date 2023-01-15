136a304eec7de89e79a6e70a019e213f1015aa6b
dubey748 <dubeyabhishek748@gmail.com>
2023-01-16 00:23:41 +0530

    https://www.youtube.com/watch?v=HrybPYpOvz0&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=22&ab_channel=AdityaVerma

---
+++ LCS/LC_Substring.js
@@ -0,0 +1,31 @@
+const LCS = function (m, n, l1, l2) {
+    let max=0;
+    let t = new Array(l1 + 1);
+    for (let i = 0; i <= l1; i++) {
+      t[i] = new Array(l2 + 1);
+      for (let j = 0; j <= l2; j++) {
+        if (i == 0 || j == 0) t[i][j] = 0;
+      }
+    }
+    for (let i = 1; i <= l1; i++) {
+      for (let j = 1; j <= l2; j++) {
+        if (m[i - 1] == n[j - 1]) {
+          t[i][j] = 1+ t[i - 1][j - 1];
+        } else {
+          t[i][j] = 0
+        }
+        if(t[i][j]>max){
+            max=t[i][j]
+        }
+      }
+    }
+    return max;
+  };
+  var longestCommonSubstring = function (text1, text2) {
+    let len1 = text1.length;
+    let len2 = text2.length;
+    return LCS(text1, text2, len1, len2);
+  };
+  
+  console.log(longestCommonSubstring("abnccccde", "abccccfce"));
+  
